b8c5effa13ec2b4f84c1f73b24e2b4fa6ca4c12c
ValleyRanch <38300259+ValleyRanch@users.noreply.github.com>
2018-04-18 12:21:56 -0500

    Inherits="Imcs_Protal_Asp.SiteMaster"

--- Imcs Protal Asp/Site.Master.designer.cs
+++ Imcs Protal Asp/Site.Master.designer.cs
@@ -22,15 +22,6 @@ namespace Imcs_Protal_Asp {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
