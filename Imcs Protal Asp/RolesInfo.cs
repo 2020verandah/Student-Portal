b7ed3235b42e9a395f3fcf9401b48e26793515b8
ValleyRanch <38300259+ValleyRanch@users.noreply.github.com>
2018-04-17 21:13:17 -0500

    added properties to rolesinfo table

---
+++ Imcs Protal Asp/RolesInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace Imcs_Protal_Asp
+{
+    public class RolesInfo
+    {
+        private int _roleid;
+        private string _rolename;
+
+
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+
+    }
+}
