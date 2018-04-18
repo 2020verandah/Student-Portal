daead71fa4c58c0c026329ffeaac52b20d8e441a
ValleyRanch <38300259+ValleyRanch@users.noreply.github.com>
2018-04-18 00:33:33 -0500

    Calling SQL helper method

--- Imcs Protal Asp/DAL.cs
+++ Imcs Protal Asp/DAL.cs
@@ -14,23 +14,9 @@ namespace Imcs_Protal_Asp
     {
         string connstring = WebConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
 
-        // SqlConnection conn = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]);
-
-        //SqlConnection conn = new SqlConnection("Server=38.84.37.193;Database=vicky;User Id=vicky_test;Password=Test@123");
+        SQLHelper sqlHelper = new SQLHelper();
         public void CreateNewAssessment(List<AssessmentInfo> assessList)
         {
-            //using (SqlConnection conn = new SqlConnection(connstring))
-            // {
-            //     SqlCommand sqlComm = new SqlCommand("usp_CreateAssessment", conn);
-            //     sqlComm.Parameters.AddWithValue("@name", assessList[0].Name);
-            //     sqlComm.Parameters.AddWithValue("@mode", assessList[0].Mode);
-            //     sqlComm.Parameters.AddWithValue("@postDate", assessList[0].Date);
-            //     sqlComm.Parameters.AddWithValue("@link", assessList[0].Link);
-            //     sqlComm.Parameters.AddWithValue("@trainer_Id", assessList[0].TrainerId);
-            //     sqlComm.Parameters.AddWithValue("@marks", assessList[0].Marks);
-            //     sqlComm.Parameters.AddWithValue("@result", 0);
-            //     //sqlComm.Parameters.Add("@marks", SqlDbType.Int).Value = assessList[0].Marks;
-            // };
             SqlParameter[] sqlparam = new SqlParameter[7];
             sqlparam[0] = new SqlParameter("@name", assessList[0].Name);
             sqlparam[1] = new SqlParameter("@mode", assessList[0].Mode);
@@ -40,12 +26,9 @@ namespace Imcs_Protal_Asp
             sqlparam[5] = new SqlParameter("@marks", assessList[0].Marks);
             sqlparam[6] = new SqlParameter("@result", 0);
 
-            SQLHelper sqlHelper = new SQLHelper();
-            int resultValue = sqlHelper.RunSql("usp_CreateAssessment", sqlparam);
-            //conn.Open();
-            //sqlComm.CommandType = CommandType.StoredProcedure;
-            //int rows=sqlComm.ExecuteNonQuery();
-            //conn.Close();
+            
+            int resultValue = sqlHelper.RunSp("usp_CreateAssessment", sqlparam);
+           
         }
 
         public DataSet gvbind()
