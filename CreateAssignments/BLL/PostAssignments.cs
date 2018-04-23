971fde7502c82777261d22c1b9f2e14bde6c30c5
ValleyRanch <38300259+ValleyRanch@users.noreply.github.com>
2018-04-23 09:10:01 -0500

    adding assignment members

---
+++ CreateAssignments/BLL/PostAssignments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAssignments.BLL
+{
+    public class PostAssignments
+    {
+        public int AssignmentID { get; set; }
+        public string AssignmentName { get; set; }
+        public string AssignmentDeadline { get; set; }
+        public string AssignmentLink { get; set; }
+        public int AssignmentMarks { get; set; }
+        public int TrainerID { get; set; }
+    }
+}
---
+++ CreateAssignments/BLL/StudentFacultyinfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAssignments.BLL
+{
+    public class StudentFacultyinfo
+    {
+        public int SID { get; set; }
+        public string SFirstName { get; set; }
+        public string SLastName { get; set; }
+
+        public int FID { get; set; }
+        public string FFirstName { get; set; }
+        public string FLastName { get; set; }
+    }
+}
