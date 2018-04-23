13aa83c0cf190fa405c60d299e542da8a1ebf4c1
ValleyRanch <38300259+ValleyRanch@users.noreply.github.com>
2018-04-23 08:20:32 -0500

    added Assessments/Assessments.js

---
+++ Imcs Protal Asp/Scripts/Assessments/Assessments.js
@@ -0,0 +1,90 @@
+function clientValidation_Click() {
+
+    if ($.trim($("#DDLAssessMode").val()) == -1) {
+        $("#DDLAssessMode").css({
+            "border": "1px solid red",
+            "background": "#FFCECE"
+        });
+    }
+    else {
+        $("#DDLAssessMode").css({
+            "border": "",
+            "background": ""
+        });
+    }
+    var isValid = true;
+    $('input[type="text"],input[type="file"]').each(function () {
+        if ($.trim($(this).val()) == '' || $.trim($(this).val()) == -1) {
+            isValid = false;
+            $(this).css({
+                "border": "1px solid red",
+                "background": "#FFCECE"
+            });
+        }
+        else {
+            $(this).css({
+                "border": "",
+                "background": ""
+            });
+        }
+    });
+    if (isValid == false)
+        return false;
+    else { }
+
+    // emailExp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([com\co\.\in])+$/;
+}
+
+$(document).ready(function () {
+   
+        //$("#txtAssessDate").datepicker();
+    //$('#datetimepicker1').datetimepicker();
+
+    $('#datetimepicker1').datetimepicker();
+
+    $("#TxtAssessName").focusout(function () {
+        clientValidation_Click();
+    });
+});
+
+
+function CreateAssessmentClient() {
+    debugger;
+    var path = '/WebService1.asmx/GetData';
+    var Assessdata = {
+        name: $('#TxtAssessName').val(),
+        mode: $('#DDLAssessMode').val(),
+        date: $('#txtAssessDate').val(),
+        link: $('#FileUploadLink').val(),
+        marks: $('#TxtMarks').val()
+    };
+    //$.ajax({
+    //    method: 'post',
+    //    url: 'WebService1.asmx/GetData',
+    //    contentType: 'application/json; charset=utf-8',
+    //    data: JSON.stringify({ objAssess: Assessdata }),
+    //    dataType: 'json',
+    //    success: function (data) {
+    //        alert(data.d);
+
+
+    //    },
+    //    error: function (request, status, error) {
+    //        alert(request);
+    //        alert(status);
+    //        alert(error);
+    //    }
+
+    //});
+    $.ajax({
+        type: "POST",
+        url: "WebService1.asmx/HelloWorld",
+        data:JSON.stringify({ objAssess: Assessdata }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data, status) { alert(data.d); },
+        error: function (request, status, error) { alert(request.responseText); }
+    });
+}
+
+
---
+++ Imcs Protal Asp/WebService1.asmx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace Imcs_Protal_Asp
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true)]
+        public static void GetData(AssessmentInfo objAssess)
+
+        {
+            List<AssessmentInfo> AssessList = new List<AssessmentInfo>();
+            AssessmentBLL objDal = new AssessmentBLL();
+            string name = objAssess.Name;
+            string mode = objAssess.Mode;
+            string date = objAssess.Date;
+            string link = objAssess.Link;
+            int marks = objAssess.Marks;
+            int trainerId = 1;
+
+            AssessList.Add(new AssessmentInfo()
+            {
+                Name = name,
+                Mode = mode,
+                Date = date,
+                Link = link,
+                Marks = marks,
+                TrainerId = trainerId
+            });
+            objDal.CreateNewAssessment(AssessList);
+            //Response.Redirect("~/ViewAssessments.aspx");
+        }
+        [WebMethod]
+        public void HelloWorld(AssessmentInfo objAssess)
+        {
+            List<AssessmentInfo> AssessList = new List<AssessmentInfo>();
+            AssessmentBLL objDal = new AssessmentBLL();
+            string name = objAssess.Name;
+            string mode = objAssess.Mode;
+            string date = objAssess.Date;
+            string link = objAssess.Link;
+            int marks = objAssess.Marks;
+            int trainerId = 1;
+
+            AssessList.Add(new AssessmentInfo()
+            {
+                Name = name,
+                Mode = mode,
+                Date = date,
+                Link = link,
+                Marks = marks,
+                TrainerId = trainerId
+            });
+            objDal.CreateNewAssessment(AssessList);
+        }
+    }
+}
+
